7e7ff9e59276c1d20a931939c79b9cae10f78d1e
Eric Brooke <ericbrooke@mac.com>
2016-06-21 09:49:28 -0400

    updated layout, wired Button and checkbox

--- app/src/main/java/com/example/ebrooke/criminalintent/Crime.java
+++ app/src/main/java/com/example/ebrooke/criminalintent/Crime.java
@@ -1,5 +1,6 @@
 package com.example.ebrooke.criminalintent;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -8,10 +9,12 @@ import java.util.UUID;
 public class Crime {
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime() {
-        // Generate unique identifer
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -24,5 +27,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
