6f337d7593be986a690150f653ed5c49d8b83679
Eric Brooke <ericbrooke@mac.com>
2016-06-22 14:33:47 -0400

    SQL DB Lite setup and wired in

--- app/src/main/java/com/example/ebrooke/criminalintent/database/CrimeBaseHelper.java
+++ app/src/main/java/com/example/ebrooke/criminalintent/database/CrimeBaseHelper.java
@@ -5,6 +5,8 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Build;
 
+import com.example.ebrooke.criminalintent.database.CrimeDbSchema.CrimeTable;
+
 /**
  * Created by ebrooke on 6/22/16.
  */
@@ -18,9 +20,16 @@ public class CrimeBaseHelper extends SQLiteOpenHelper{
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
+        db.execSQL("create table " + CrimeTable.NAME + "(" +
+        " _id integer primary key autoincrement, " +
+                CrimeTable.Cols.UUID + ", " +
+                CrimeTable.Cols.TITLE + ", " +
+                CrimeTable.Cols.DATE + ", " +
+                CrimeTable.Cols.SOLVED +
+                ")" );
     }
-        @Override
+
+    @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVerion) {
 
         }
