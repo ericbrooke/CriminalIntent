4d522d71e4a262ba45be784d6e5eb1ce91056545
Eric Brooke <ericbrooke@mac.com>
2016-06-22 15:31:04 -0400

    can now send Crime report to other apps

--- app/src/main/java/com/example/ebrooke/criminalintent/CrimeFragment.java
+++ app/src/main/java/com/example/ebrooke/criminalintent/CrimeFragment.java
@@ -33,6 +33,7 @@ public class CrimeFragment extends Fragment {
     private EditText mTitleField;
     private Button mDateButton;
     private CheckBox mSolvedCheckBox;
+    private Button mReportButton;
 
     public static CrimeFragment newInstance(UUID crimeId) {
         Bundle args = new Bundle();
@@ -99,9 +100,22 @@ public class CrimeFragment extends Fragment {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 mCrime.setSolved(isChecked);
-            }
-    });
 
+            }
+         });
+
+        mReportButton = (Button) v.findViewById(R.id.crime_report);
+        mReportButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("text/plain");
+                i.putExtra(Intent.EXTRA_TEXT, getCrimeReport());
+                i.putExtra(Intent.EXTRA_SUBJECT,
+                        getString(R.string.crime_report_subject));
+                i = Intent.createChooser(i, getString((R.string.send_report)));
+                startActivity(i);
+            }
+        });
         return v;
     }
     @Override
